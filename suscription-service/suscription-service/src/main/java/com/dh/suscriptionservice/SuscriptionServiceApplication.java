982006b5603273829de4a570363c0db5da6d0839
StefanySB <stefany9209@gmail.com>
2023-06-19 17:52:42 -0500

    primer commit microservicios andando la basico, solo con modelos sin bases ni nada mas

---
+++ suscription-service/suscription-service/src/main/java/com/dh/suscriptionservice/SuscriptionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dh.suscriptionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuscriptionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuscriptionServiceApplication.class, args);
+	}
+
+}
---
+++ suscription-service/suscription-service/src/test/java/com/dh/suscriptionservice/SuscriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.suscriptionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuscriptionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
