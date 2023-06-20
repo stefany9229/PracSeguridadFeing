78e13503a3fa658305e11f741309cc796251ce58
StefanySB <stefany9209@gmail.com>
2023-06-20 00:24:10 -0500

    Funciona con el feign que configura para obtener el token del contexto de seguridad

--- user-service/user-service/src/main/java/com/dh/userservice/repository/FeignSubscriptionRepository.java
+++ user-service/user-service/src/main/java/com/dh/userservice/repository/FeignSubscriptionRepository.java
@@ -2,13 +2,14 @@ package com.dh.userservice.repository;
 
 
 import com.dh.userservice.model.SubscriptionDTO;
+import com.dh.userservice.security.feign.FeignInterceptor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name= "subscription-service",url = "http://localhost:8083")
+@FeignClient(name= "subscription-service",url = "http://localhost:8083",configuration = FeignInterceptor.class)
 public interface FeignSubscriptionRepository {
 
     @RequestMapping(method = RequestMethod.GET,value = "/subscription/find")
