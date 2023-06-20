89e32b6caafeb23d77bfb9efca70d81e85429e2a
StefanySB <stefany9209@gmail.com>
2023-06-20 09:30:14 -0500

    las dos formas de auntenticacion corren bn

--- user-service/user-service/src/main/java/com/dh/userservice/repository/FeignSubscriptionRepository.java
+++ user-service/user-service/src/main/java/com/dh/userservice/repository/FeignSubscriptionRepository.java
@@ -2,6 +2,7 @@ package com.dh.userservice.repository;
 
 
 import com.dh.userservice.model.SubscriptionDTO;
+import com.dh.userservice.security.feign.FeignInterceptor;
 import com.dh.userservice.security.feign.OAuthFeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name= "subscription-service",url = "http://localhost:8083",configuration = OAuthFeignConfig.class)
+@FeignClient(name= "subscription-service",url = "http://localhost:8083",configuration = FeignInterceptor.class)
 public interface FeignSubscriptionRepository {
 
     @RequestMapping(method = RequestMethod.GET,value = "/subscription/find")
